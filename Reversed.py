d5a43a260cc61ba9d5340ce7a423b13cebf20e6b
Pavan Kumar V <103502790+Pavan-477@users.noreply.github.com>
2022-10-23 16:09:51 +0530

    Delete Reversed.py

--- Reversed.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program that accepts a word from the user and reverse it.
-seq=input("Enter a string to reverse :")
-reversed_seq = ''
-for i in seq:
-    reversed_seq = i + reversed_seq
-print(reversed_seq)
